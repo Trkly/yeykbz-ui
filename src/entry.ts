5f3e11c31424843a89b796301c485fdf2469cb51
zzk <Acapplella@163.com>
2025-03-05 20:58:47 +0800

    fix(common): 继续处理组件导入问题

--- src/entry.ts
+++ src/entry.ts
@@ -1,15 +1,21 @@
+// src/entry.ts
 import "virtual:uno.css";
 import type { App } from "vue";
 import { Button } from "./Button";
 import { Card } from "./Card";
 
-// 编写一个插件，实现一个install方法
-// export { YButton };
-// export { YCard };
-const Yeykbz = {
-  install(app: App): void {
-    app.component(Button?.name || 'YButton', Button);
-    app.component(Card?.name || 'YCard', Card);
-  },
+// 定义插件安装方法
+const install = (app: App) => {
+  app.component(Button?.name || 'YButton', Button);
+  app.component(Card?.name || 'YCard', Card);
 };
-export default Yeykbz
+
+// 导出命名组件
+// export { Button as YButton, Card as YCard };
+
+// 导出默认插件对象
+export default {
+  install,
+  YButton: Button,
+  YCard: Card
+};
