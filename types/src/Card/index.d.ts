2ad34c6d9afc1490c62f103a40041aff11c8ab89
Trkly <79641247+Trkly@users.noreply.github.com>
2025-04-07 18:15:05 +0000

    Deploying to gh-pages from @ Trkly/yeykbz-ui@b56e8cd58d04b80a80d236dade567feca8aa2bb9 🚀

---
+++ types/src/Card/index.d.ts
@@ -0,0 +1,14 @@
+export declare const YCard: {
+    new (...args: any[]): import("vue").CreateComponentPublicInstanceWithMixins<Readonly<import("vue").ExtractPropTypes<{}>>, {}, {}, import("vue").ComputedOptions, import("vue").MethodOptions, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, import("vue").PublicProps, {}, true, {}, {}, import("vue").GlobalComponents, import("vue").GlobalDirectives, string, {}, any, import("vue").ComponentProvideOptions, {
+        P: {};
+        B: {};
+        D: {};
+        C: {};
+        M: {};
+        Defaults: {};
+    }, Readonly<import("vue").ExtractPropTypes<{}>>, {}, {}, import("vue").ComputedOptions, import("vue").MethodOptions, {}>;
+    __isFragment?: never;
+    __isTeleport?: never;
+    __isSuspense?: never;
+} & import("vue").ComponentOptionsBase<Readonly<import("vue").ExtractPropTypes<{}>>, {}, {}, import("vue").ComputedOptions, import("vue").MethodOptions, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, {}, {}, string, {}, import("vue").GlobalComponents, import("vue").GlobalDirectives, string, import("vue").ComponentProvideOptions> & import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps & import("vue").Plugin;
+export default YCard;
---
+++ types/src/install.d.ts
@@ -0,0 +1,6 @@
+import type { Plugin } from "vue";
+export declare const install: Plugin["install"];
+export declare const YeykbzUI: {
+    install: (app: import("vue").App, ...options: any[]) => any;
+    version: string;
+};
